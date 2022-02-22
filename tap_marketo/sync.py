48400132a20c703ed2517307e00d6de5699efc22
Brent Piephoff <brent.piephoff@gmail.com>
2022-02-22 12:58:19 -0800

    trying to retrieve custom marketo fields

--- tap_marketo/sync.py
+++ tap_marketo/sync.py
@@ -383,12 +383,13 @@ def sync_leads_paginated(client, state, stream):
     # This is a supplementary sync for Lead. We're Marketo Static List 13702 to give us newly updated leads
     # Leads are paginated with a max return of 300 items per page 
     replication_key = determine_replication_key(stream['tap_stream_id'])
-    fields = "id, firstName, lastName, email, updatedAt, createdAt, sfdcContactId, sfdcLeadId, sfdcId, mktoName, phone, mobilePhone, postalCode, address"
-    # singer.log_info(f"Lead fields is :{fields.keys()}")
+    fields = ",".join(list(stream["schema"]["properties"].keys()))
+    singer.log_info(f"Lead fields is :{fields}")
+    raise
 
     singer.write_schema(stream["tap_stream_id"], stream["schema"], stream["key_properties"], bookmark_properties=[replication_key])
     start_date = bookmarks.get_bookmark(state, stream["tap_stream_id"], replication_key)
-    params = {"batchSize": 300, "fields": fields}
+    params = {"batchSize": 300, "fields": ",".join(fields)}
     
     # https://engage-ab.marketo.com/?munchkinId=739-BIK-855#/classic/ST13702A1LA1
     static_supplement_lead_list = 13702
