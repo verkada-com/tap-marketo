3d9ae76c424277e02e1a3cc34fa649b8809e91e2
Brent Piephoff <brent.piephoff@gmail.com>
2022-02-23 17:45:44 -0800

    debugging leads pagination large list

--- tap_marketo/sync.py
+++ tap_marketo/sync.py
@@ -431,7 +431,8 @@ def sync_leads_paginated(client, state, stream):
 
     # Once all results are exhausted, unset the next page token bookmark
     # so the subsequent sync starts from the beginning.
-    state = bookmarks.write_bookmark(state, stream["tap_stream_id"], "next_page_token", None)
+    #TODO - Uncomment this after debugging why we can't iterate through entire list...
+    # state = bookmarks.write_bookmark(state, stream["tap_stream_id"], "next_page_token", None)
     state = bookmarks.write_bookmark(state, stream["tap_stream_id"], replication_key, job_started)
     singer.write_state(state)
     return state, record_count
