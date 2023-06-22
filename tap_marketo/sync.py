428eaabf5980e4fed015c09486ee66a10d91adc9
Brent Piephoff <brent.piephoff@gmail.com>
2023-06-21 19:43:37 -0700

    adding merges activity

--- tap_marketo/sync.py
+++ tap_marketo/sync.py
@@ -619,6 +619,8 @@ def sync(client, catalog, config, state):
             state, record_count = sync_activities_paginated(client, state, stream, 131)
         elif stream["tap_stream_id"] == "activities_delete_lead":
             state, record_count = sync_activities_paginated(client, state, stream, 37)
+        elif stream["tap_stream_id"] == "activities_merge_leads":
+            state, record_count = sync_activities_paginated(client, state, stream, 32)
             
         # elif stream["tap_stream_id"].startswith("activities_"):
         #     state, record_count = sync_activities(client, state, stream, config)
